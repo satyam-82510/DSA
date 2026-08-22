0218eea4918b0d7f1a479de80dbd39174bc70bda
Satyam Vishwakarma <techyouji@gmail.com>
2026-08-22 01:12:51 -0500

    Time: 2 ms (99.33%), Space: 47 MB (59.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,6 @@
 // TWO VARIETY ASK IN TWO SUM   1.SAY YES OR NO     2. SAY THE INDEX OF IF YES
 
-//1. BRUTE FORCE APPROACH 
+//1. BRUTE FORCE APPROACH [MULTI LOOP APPROACH]
 /*
 class Solution {
       public int[] twoSum(int[] nums, int target) {
@@ -25,9 +25,10 @@ class Solution {
 
 
 
-// 2. BETTER OR OPTIMAL ACCORDING TO ITS VARIETY OF TWO SUM ASK 
+// 2. BETTER OR OPTIMAL ACCORDING TO ITS VARIETY OF TWO SUM ASK : [HASHMAP OR HASHING APPROACH]
+// It is best for second variety of question
 
-/*
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -46,73 +47,32 @@ class Solution {
         return new int[]{}; // Fallback if no solution found
     }
 }
-*/
 
 
-//3. OPTIMAL FOR FIRST VARIETY ONLY
-//NOT RUN
+
+//3. OPTIMAL FOR FIRST VARIETY :[TWO POINTER APPROACH] (Note: sorting is required)
 /*
-import java.util.Arrays;
-
-class Solution { 
-    public boolean twoSumExist(int[] nums, int target) { 
-        Arrays.sort(nums); 
-        int left = 0; 
-        int right = nums.length - 1; 
-        
-        while (left < right) { 
-            int sum = nums[left] + nums[right]; 
-            
-            if (sum == target) { 
-                return true; 
-            } else if (sum < target) { 
-                left++; 
-            } else { 
-                right--; 
-            } 
-        } 
-        return false; 
-    } 
-}
-*/
-
-
-//RUNNABLE CODE
-
 import java.util.Arrays;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
+      Arrays.sort(nums);
+        int i = 0, j = nums.length - 1;
 
-        // 1D array storing indices: {0, 1, 2, ..., n - 1}
-        Integer[] index = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            index[i] = i;
-        }
+        while (i < j) {
+            int sum = nums[i] + nums[j];
 
-        // Sort the index array based on corresponding values in nums
-        Arrays.sort(index, (a, b) -> Integer.compare(nums[a], nums[b]));
-
-       // CONCEPT
-        // Two-pointer search using the sorted indices
-        int left = 0;
-        int right = n - 1;
-
-        while (left < right) {
-            int sum = nums[index[left]] + nums[index[right]];
-
-            if (sum == target) {
-                return new int[] { index[left], index[right] };
+            if (sum > target) {
+                j--;
             } else if (sum < target) {
-                left++;
+                i++;
             } else {
-                right--;
+                return new int[]{i, j};
             }
         }
 
-        return new int[] {};
+        return new int[]{};
     }
 }
 
-
+*/
