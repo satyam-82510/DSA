f02548d0f77cb033d9afbe57752689ae8132c24d
Satyam Vishwakarma <techyouji@gmail.com>
2026-08-22 02:34:44 -0500

    Time: 1912 ms (5.11%) | Memory: 55.4 MB (69.05%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,38 +1,17 @@
 class Solution {
-    // Function to find the majority element in an array
     public int majorityElement(int[] nums) {
-        
-        // Size of the given array
         int n = nums.length;
-        
-        // Iterate through each element of the array
-        for (int i = 0; i < n; i++) {
-            
-            // Counter to count occurrences of nums[i]
-            int cnt = 0; 
-            
-            // Count the frequency of nums[i] in the array
-            for (int j = 0; j < n; j++) {
-                if (nums[j] == nums[i]) {
-                    cnt++;
-                }
+              for (int i=0;i<n;i++){
+                int count= 0; 
+                for (int j=0;j<n; j++){
+                    if(nums[i]==nums[j]){
+                     count++;
+                    }
+                }    
+            if (count > n/2){
+                return nums[i];
             }
-            
-            // Check if frequency of nums[i] is greater than n/2
-            if (cnt > (n / 2)) {
-                // Return the majority element
-                return nums[i]; 
             }
-        }
-        
-        // Return -1 if no majority element is found
-        return -1; 
+    return -1; 
     }
-}
-
-
-
-
-
-
-  
+}  
