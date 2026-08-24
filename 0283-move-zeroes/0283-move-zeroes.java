45d98c52290f91d8a45b24ea89e5fdd750fc151c
Satyam Vishwakarma <techyouji@gmail.com>
2026-08-24 05:19:31 -0500

    Time: 2 ms (91.96%), Space: 47.7 MB (57.67%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // Pointer to the first zero
+        int j = -1;
+
+        // Find the first zeroth index
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        // If no zero found, return
+        if (j == -1) return;
+
+        // Start from the next index of first zero
+        for (int i = j + 1; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                // Swap with nums[j]
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
