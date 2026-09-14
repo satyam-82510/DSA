cef12724b8718a1dc7d810b76ffd9512eed207b5
Satyam Vishwakarma <techyouji@gmail.com>
2026-09-14 14:39:55 -0500

    Time: 3 ms (99.81%), Space: 46.2 MB (90.24%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -39,7 +39,7 @@ class Solution {
     int value[]= {1000,900,500,400,100,90,50,40,10,9,5,4,1};
     String[] romanletters = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
                     
-//   CORRECTED BUT SLOW:  for(int i=0;i<value.length;i++){
+//   CORRECT BUT SLOW:  for(int i=0;i<value.length;i++){
 //         while(num>=value[i]){
 //         roman += romanletters[i];
 //         num -= value[i];
