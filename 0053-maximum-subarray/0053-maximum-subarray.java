82bf5d9b4f15bb655045495fe43566fd19491416
Satyam Vishwakarma <techyouji@gmail.com>
2026-09-17 06:56:00 -0500

    Time: 1 ms (99.93%), Space: 80.7 MB (5.87%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+    long ms=Long.MIN_VALUE;
+    long sum= 0;
+    int n = nums.length;
+    for(int i=0;i<n;i++){
+    sum += nums[i];
+    if (sum>ms){
+        ms=sum;
+    }
+    if(sum<0){
+        sum=0;
+    }
+    }
+    // return ms; give error as lossy conversion as function return a need of int return answer value in int
+    return (int) ms;
+    }
+}
