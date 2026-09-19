fd0909492c6e005c010c063c49a83f541b78e10e
Satyam Vishwakarma <techyouji@gmail.com>
2026-09-19 10:02:34 -0500

    Time: 0 ms (100%), Space: 41.8 MB (93.24%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -23,7 +23,7 @@
 //3. using dp fastest
 class Solution {
     public int fib(int n) {
-        if(n==0) return 0;  //yeh dono line ke bina bhi run ho sakta hai
+        if(n==0) return 0;  //yeh dono line ke bina run ho jayega but but note ArrayINDEXOUTOFBOUND COMPILATION ERROR GIVE
         if(n==2 || n==1) return 1;
         int[] dp=new int[n+1];
         dp[0]=0;
